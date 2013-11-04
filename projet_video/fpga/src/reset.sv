588a101c75ef035725ca1d396832791d5fcc271a
Gu3nol - Guénolé LALLEMENT <guenole.lallement@gmail.com>
2013-11-04 16:55:37 +0100

    modification mineure

--- projet_video/fpga/src/reset.sv
+++ projet_video/fpga/src/reset.sv
@@ -14,23 +14,11 @@ module reset #(parameter rst_activity = 1)(input   CLK, NRST,
 
    logic              r0;
 
-
    /* Création d'un RESET asynchrone stable */
    always_ff @(posedge CLK or negedge NRST)
      begin
-        if(~NRST)
-          begin
-             resync_rst <= rst_activity;
-             r0 <= rst_activity;
-          end
-        else
-          begin
-             resync_rst <= r0;
-             r0 <= ~rst_activity;
-          end
+        if(~NRST) {resync_rst,r0} <= {rst_activity, rst_activity};
+        else  {resync_rst,r0} <= {r0,~rst_activity};
      end
    // Fin RESET aysnchrone stable
-
-
-
 endmodule // reset
