6b63e39445df51f59737e3b098469eaa5aaf58f4
tassinari <olivier.tassinari@telecom-paristech.fr>
2013-11-07 17:37:33 +0100

    WIP

--- projet_video/vga/src/vga.sv
+++ projet_video/vga/src/vga.sv
@@ -1,3 +1,12 @@
-module vga ()
+module vga (CLK, RST, VGA_CLK, VGA_HS, VGA_VS, VGA_BLANK, VGA_SYNC)
+
+   input CLK, RST;
+   output VGA_CLK, VGA_HS, VGA_VS, VGA_BLANK, VGA_SYNC;
+
+   always_comb
+     begin
+        VGA_SYNC = 0;
+        VGA_CLK = CLK;
+     end
 
 endmodule
