f3a661a3cb0568bbcdb680cdd78a6aab26342bfe
tassinari <olivier.tassinari@telecom-paristech.fr>
2013-11-07 17:40:48 +0100

    WIP

--- projet_video/vga/src/vga.sv
+++ projet_video/vga/src/vga.sv
@@ -1,5 +1,14 @@
 module vga (CLK, RST, VGA_CLK, VGA_HS, VGA_VS, VGA_BLANK, VGA_SYNC)
 
+   localparam HDISP = 640;
+   localparam VDISP = 480;
+   localparam HFP = 16;
+   localparam HPULSE = 96;
+   localparam HBP = 48;
+   localparam VFP = 11;
+   localparam VPULSE = 2;
+   localparam VBP = 31;
+
    input CLK, RST;
    output VGA_CLK, VGA_HS, VGA_VS, VGA_BLANK, VGA_SYNC;
 
