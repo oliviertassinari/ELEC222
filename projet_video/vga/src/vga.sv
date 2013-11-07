a0f675527ca202c1cae14077537e6a7dffe96c9a
tassinari <olivier.tassinari@telecom-paristech.fr>
2013-11-07 17:47:44 +0100

    WIP

--- projet_video/vga/src/vga.sv
+++ projet_video/vga/src/vga.sv
@@ -10,6 +10,9 @@ module vga #(parameter HDISP = 6440, VDISP = 480)(CLK, RST, VGA_CLK, VGA_HS, VGA
    input CLK, RST;
    output VGA_CLK, VGA_HS, VGA_VS, VGA_BLANK, VGA_SYNC;
 
+   logic rst_async;
+   reset #(.is_nrst('b0)) reset_i(.CLK(VGA_CLK), .RST(RST), .rst_async(rst_async));
+
    always_comb
      begin
         VGA_SYNC = 0;
