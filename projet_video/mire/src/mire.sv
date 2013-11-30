cb1039374b995502d3c4ed3841199e0d81e631f0
Olivier Tassinari <you@example.com>
2013-11-30 19:42:04 +0100

    mire : ok

--- projet_video/mire/src/mire.sv
+++ projet_video/mire/src/mire.sv
@@ -5,21 +5,26 @@ module mire #(parameter HDISP = 640, VDISP = 480)(
     localparam logic [$clog2(HDISP*VDISP)-1:0] NBPIX = HDISP*VDISP;
 
     logic [$clog2(NBPIX)-1:0]               ctMire;
-    logic                                   mire_loaded
+    logic                                   mire_loaded, cyc;
 
     always_ff @(posedge wb_m.clk)
     begin
        if(wb_m.rst)
        begin
             mire_loaded <= 0;
+            cyc <= 1;
             ctMire <= '0;
        end
        else
           if(!mire_loaded)
             begin
+                if(cyc == 0)
+                    cyc <= 1;
+
                if(wb_m.ack)
                  begin
                     ctMire <= ctMire + 1'b1;
+                    cyc <= 0;
 
                     if(ctMire == NBPIX - 1'b1)
                       begin
@@ -32,7 +37,7 @@ module mire #(parameter HDISP = 640, VDISP = 480)(
 
     always_comb
     begin
-        wb_m.cyc = 1;
+        wb_m.cyc = 0;
         wb_m.sel = 2'b11;
         wb_m.cti = '0;
         wb_m.bte = '0;
@@ -43,6 +48,7 @@ module mire #(parameter HDISP = 640, VDISP = 480)(
 
         if(!mire_loaded)
           begin
+             wb_m.cyc = cyc;
              wb_m.adr = 2*ctMire;
              wb_m.dat_ms = { 6'b0, ctMire[9:0]};
              wb_m.stb = 1;
